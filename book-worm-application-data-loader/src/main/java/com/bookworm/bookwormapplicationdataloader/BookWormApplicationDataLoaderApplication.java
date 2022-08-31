9dc2fae6130abe5226b98b9aea3abef494e9f2ef
Sumit Kumar <sumitkishore0131@gmail.com>
2022-08-31 17:50:51 +0530

    intial data loader setup

---
+++ book-worm-application-data-loader/src/main/java/com/bookworm/bookwormapplicationdataloader/BookWormApplicationDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.bookworm.bookwormapplicationdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookWormApplicationDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookWormApplicationDataLoaderApplication.class, args);
+	}
+
+}
---
+++ book-worm-application-data-loader/src/test/java/com/bookworm/bookwormapplicationdataloader/BookWormApplicationDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookworm.bookwormapplicationdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookWormApplicationDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
